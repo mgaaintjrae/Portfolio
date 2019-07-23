1a5987a3d6a2acfddf9799c1b58b3c2d42849a57
mgaaintjrae <pr.priscilla.roy@gmail.com>
2019-07-23 14:48:09 +0000

    parallax header improvement, parallax footer check

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,15 @@
 console.log("hello");
 
-const header = document.querySelector('.home-header-background');
-const headerContent = header.querySelector('.home-header-background-content');
+// const header = document.querySelector('.home-header-background');
+// const headerContent = header.querySelector('.home-header-background-content');
 
-window.addEventListener('scroll', e => {
-  headerContent.style.top = `${50 + (window.pageYOffset / 16)}%`
+
+// window.addEventListener('scroll', e => {
+//     headerContent.style.top = `${50 + (window.pageYOffset / 16)}%`
+// });
+
+const parallax = document.querySelector('.parallax');
+
+window.addEventListener('scroll', () => {
+    parallax.style.backgroundPositionY = -window.scrollY / 4 + "px";
 });
